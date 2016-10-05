f3e8659fa50743499979ae28959ec5a058272317
romaswe <romaswe@romaswe-Lenovo-Y50-70>
2016-10-05 21:19:49 +0200

    Fixat topnav så den är helt skalbar, bytt namn på en av css filerna för att skapa mindre förvirring i vilken man ska skriva css i, få se om sido menyn kan fixas i mobilläge eller om den tas bort

--- web/resources/js/show.js
+++ web/resources/js/show.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
     $('.scrollspy').scrollSpy();
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
     $('.materialboxed').materialbox();
+    $(".button-collapse").sideNav();
 });
