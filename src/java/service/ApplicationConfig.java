e14c7f87897cbbf6ac11fc3fbe0045560bdf8775
Sebastian F <forsthsebbe@hotmail.com>
2016-10-11 11:30:42 +0200

    Fixat att text klipptesbort vid mobilskalning och lagt till plats för förrätt,huvudrätt och efterrätt

--- src/java/service/ApplicationConfig.java
+++ src/java/service/ApplicationConfig.java
@@ -29,6 +29,11 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(service.ApploginFacadeREST.class);
+        resources.add(service.DailyspecialFacadeREST.class);
+        resources.add(service.DishFacadeREST.class);
+        resources.add(service.DrinkFacadeREST.class);
+        resources.add(service.UserloginFacadeREST.class);
     }
     
 }
