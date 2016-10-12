756cf4caa65b614159797974aeb7fb3eee04e2dd
mojjie <mojjie@10.250.118.65>
2016-10-12 15:23:05 +0200

    William och Marcus lilla textfixning!

---
+++ src/java/se/miun/dt142g/ImagetextEJB.java
@@ -0,0 +1,35 @@
+
+package se.miun.dt142g;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import se.miun.dt142g.model.Imagetext;
+
+/**
+ *
+ * @author William
+ */
+@Stateless
+@Named
+public class ImagetextEJB {
+
+@PersistenceContext
+    EntityManager em;
+    
+    @Inject Imagetext imagetext;
+    
+    public List<Imagetext> getList(){
+        return em.createNamedQuery("Imagetext.findAll").getResultList();
+    }
+    public void create(){
+        Imagetext d = new Imagetext(imagetext.getImagetext());
+        em.persist(d);
+    }
+    public void delete(){
+        em.createQuery("DELETE FROM Imagetext c WHERE c.imagetextid = " + imagetext.getImagetext()).executeUpdate();
+    }
+}
---
+++ src/java/se/miun/dt142g/model/Imagetext.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.miun.dt142g.model;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author mojjie
+ */
+@Named
+@RequestScoped
+@Entity
+@Table(name = "IMAGETEXT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Imagetext.findAll", query = "SELECT i FROM Imagetext i"),
+    @NamedQuery(name = "Imagetext.findByImagetext", query = "SELECT i FROM Imagetext i WHERE i.imagetext = :imagetext")})
+public class Imagetext implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 500)
+    @Column(name = "IMAGETEXT")
+    private String imagetext;
+
+    public Imagetext() {
+    }
+
+    public Imagetext(String imagetext) {
+        this.imagetext = imagetext;
+    }
+
+    public String getImagetext() {
+        return imagetext;
+    }
+
+    public void setImagetext(String imagetext) {
+        this.imagetext = imagetext;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (imagetext != null ? imagetext.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Imagetext)) {
+            return false;
+        }
+        Imagetext other = (Imagetext) object;
+        if ((this.imagetext == null && other.imagetext != null) || (this.imagetext != null && !this.imagetext.equals(other.imagetext))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "se.miun.dt142g.model.Imagetext[ imagetext=" + imagetext + " ]";
+    }
+    
+}
